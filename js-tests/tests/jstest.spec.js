ed0f6328fa6770eb47c16b198b3f5f87f3109d1b
Denis Palnitsky <denis.palnitsky@aurea.com>
2019-06-13 00:01:40 +0200

    QUnit unit-test

---
+++ js-tests/tests/jstest.spec.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    /*global AreaSelector */
+
+    QUnit.module('sample.test');
+    
+    QUnit.test('Success Test Example', function( assert ) {
+        assert.ok("ok");
+    });
+}());
